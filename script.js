394468d1055789c04049396bfd1276610910e2fa
Foxbarr <andrewmscott2@hotmail.com>
2022-12-27 12:31:37 +0000

    created 'round' function with if statements to determine who wins each round. Added console.log lines to show computers choice and your choice and then called the round function at the end

--- script.js
+++ script.js
@@ -5,10 +5,34 @@ return items[Math.floor(Math.random()*items.length)];
      
 }
 
-var items = ["Rock", "Paper", "Scissors"];
-let playerSelection = prompt("Please type in your choice!", "Choice");
+var items = ["rock", "paper", "scissors"];
+let playerSelection = prompt("Please type in your choice!", "Choice").toLowerCase();
 let computerSelection = random_item(items);
 
 function round(){
-    
+    if(playerSelection === computerSelection){
+        console.log("Tie Game!");
+    }
+    else if(playerSelection == "paper" && computerSelection == "rock"){
+        console.log("You won! Paper beats Rock!");
+    }
+    else if(playerSelection == "scissors" && computerSelection == "paper"){
+        console.log("You won! Scissors beats Paper!");
+    }
+    else if(playerSelection == "rock" && computerSelection == "scissors"){
+        console.log("You won! Rock beats Scissors");
+    }
+    else if(playerSelection == "rock" && computerSelection == "paper"){
+        console.log("Computer Wins! Paper beats Rock!");
+    }
+    else if(playerSelection == "paper" && computerSelection == "scissors"){
+        console.log("Computer Wins! Scissors beats Paper!");
+    }
+    else if(playerSelection == "scissors" && computerSelection == "rock"){
+        console.log("Computer Wins! Rock beats Scissors!");
+    }
+
 }
+console.log("Computer chose " + computerSelection);
+console.log(playerSelection);
+round();
