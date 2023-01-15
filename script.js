6a4e8765f9d8abdbca33e6437398d854cbf2f70e
Foxbarr <andrewmscott2@hotmail.com>
2023-01-15 22:53:30 +0000

    buttons now assign correct playerSelection and play a round. When either the win score or lose score reaches 5, the event listeners for the buttons are removed and player told to refresh page to play again

--- script.js
+++ script.js
@@ -19,41 +19,51 @@ return items[Math.floor(Math.random()*items.length)];
 
 //function to play one round and output result
 function round(){
-    
-
-    buttonRock.onclick = function(){
-        playerSelection = playerOptions[0];
-    };
-    buttonPaper.onclick = function(){
-        playerSelection = playerOptions[1];
-    };
-    buttonScissors.onclick = function(){
-        playerSelection = playerOptions[2];
-    };
-
     if(playerSelection == computerSelection)
     {
         tie++;
         tieScore.textContent = 'Ties: ' + tie;
+        resultDisplay.textContent = "It's a tie!";           
         return tie;
     }
     else if(playerSelection == "paper" && computerSelection == "rock" || playerSelection == "scissors" && computerSelection == "paper" || playerSelection == "rock" && computerSelection == "scissors")
     {
         win++;
         winScore.textContent = 'Player Score: ' + win;
-        return win;
+        resultDisplay.textContent = "You Win! " + playerSelection + " beats " + computerSelection;
+
+            if(win == maxScore){
+                resultDisplay.textContent = "You won the game! Refresh the page to play again.";
+                buttonRock.removeEventListener('click', round);
+                buttonPaper.removeEventListener('click', round);
+                buttonScissors.removeEventListener('click', round);
+            }
+            else{return;}
+        return;    
     }
     else if(playerSelection == "rock" && computerSelection == "paper" || playerSelection == "paper" && computerSelection == "scissors" || playerSelection == "scissors" && computerSelection == "rock")
     {
         lose++;
         loseScore.textContent = 'Computer Score: ' + lose;
-        return lose;
+        resultDisplay.textContent = "You Lose! " + computerSelection + " beats " + playerSelection;
+        if(lose == maxScore){
+            resultDisplay.textContent = "You lost the game! Refresh the page to play again.";
+            buttonRock.removeEventListener('click', round);
+            buttonPaper.removeEventListener('click', round);
+            buttonScissors.removeEventListener('click', round);}
+        else{return;}
+        return;
     }
 
- 
-}
+    
+    }
+    
+//displaying result on screen
+const gameResult = document.querySelector('#result');
 
+const resultDisplay = document.createElement('h3');
 
+gameResult.appendChild(resultDisplay);
 
 
 //adding buttons to screen
@@ -62,14 +72,17 @@ const container = document.querySelector('#container');
 
 const buttonRock = document.createElement('button');
 buttonRock.textContent = "Rock";
+buttonRock.addEventListener('click', function() { playerSelection = playerOptions[0];});
 buttonRock.addEventListener('click', round);
 
 const buttonPaper = document.createElement('button');
 buttonPaper.textContent = "Paper";
+buttonPaper.addEventListener('click', function(){playerSelection = playerOptions[1];});
 buttonPaper.addEventListener('click', round);
 
 const buttonScissors = document.createElement('button');
 buttonScissors.textContent = "Scissors";
+buttonScissors.addEventListener('click', function(){playerSelection = playerOptions[2];});
 buttonScissors.addEventListener('click', round);
 
 container.appendChild(buttonRock);
